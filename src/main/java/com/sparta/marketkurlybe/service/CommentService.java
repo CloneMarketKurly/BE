f08c63e63da69b873a16c8c250875b156105addc
EunhyeSong <dmsgp8292@gmail.com>
2022-04-16 17:07:25 +0900

    수정

--- src/main/java/com/sparta/marketkurlybe/service/CommentService.java
+++ src/main/java/com/sparta/marketkurlybe/service/CommentService.java
@@ -2,7 +2,9 @@ package com.sparta.marketkurlybe.service;
 
 import com.sparta.marketkurlybe.dto.CommentDto;
 import com.sparta.marketkurlybe.model.Comment;
+import com.sparta.marketkurlybe.model.Item;
 import com.sparta.marketkurlybe.repository.CommentRepository;
+import com.sparta.marketkurlybe.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,29 +13,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CommentService {
 
-//    private final DetailRepository detailRepository;
-
+    private final ItemRepository itemRepository;
     private final CommentRepository commentRepository;
 
-    public Comment findComment (int id){
+    public Comment findComment (Long id){
         return commentRepository.findById(id).orElseThrow(
                 ()-> new IllegalArgumentException("일치하는 후기가 없습니다.")
         );
     }
 
-//    @Transactional
-//    public Comment commentIn(int itemId, CommentDto commentDto){
-//        Detail detail = detailRepository.findById(itemId).orElseThrow(
-//                ()-> new IllegalArgumentException("일치하는 상품 상세페이지가 없습니다.")
-//        );
-//        Comment comment = new Comment(commentDto);
-//        comment.setDetail(detail);
-//        commentRepository.save(comment);
-//        return comment;
-//    }
+    @Transactional
+    public Comment commentIn(Long itemId, CommentDto commentDto){
+        Item item = itemRepository.findById(itemId).orElseThrow(
+                ()-> new IllegalArgumentException("일치하는 상품 상세페이지가 없습니다.")
+        );
+        Comment comment = new Comment(commentDto);
+        comment.setItem(item);
+        commentRepository.save(comment);
+        return comment;
+    }
 
     @Transactional
-    public void updateComment (int id, CommentDto commentDto) {
+    public void updateComment (Long id, CommentDto commentDto) {
         Comment comment = findComment(id);
         comment.setComment(commentDto.getComment());
         comment.setUserName(commentDto.getUserName());
