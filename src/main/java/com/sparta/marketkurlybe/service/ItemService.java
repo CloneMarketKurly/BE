68a8d81cfaf68008b8f8244cae24fb1de561e6a9
Hyun Woong <98807506+hyun-woong@users.noreply.github.com>
2022-04-17 18:10:13 +0900

    오류 난 부분 수정...

--- src/main/java/com/sparta/marketkurlybe/service/ItemService.java
+++ src/main/java/com/sparta/marketkurlybe/service/ItemService.java
@@ -24,6 +24,7 @@ public class ItemService {
         return itemList;
     }
 
+
     //상세페이지 조회
     public Map<String, Object> getItemDetails(Long itemId) {
        Item items = itemRepository.findById(itemId).orElseThrow(
