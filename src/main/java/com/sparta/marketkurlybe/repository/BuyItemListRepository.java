8c321ca64f221606e84613a8f9f1d3406b945a1a
Hyun Woong <98807506+hyun-woong@users.noreply.github.com>
2022-04-21 17:51:16 +0900

    장바구니 저장 수정

--- src/main/java/com/sparta/marketkurlybe/repository/BuyItemListRepository.java
+++ src/main/java/com/sparta/marketkurlybe/repository/BuyItemListRepository.java
@@ -4,7 +4,10 @@ import com.sparta.marketkurlybe.model.BuyItemList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BuyItemListRepository extends JpaRepository<BuyItemList, Long> {
     List<BuyItemList> findByUser_Id(Long userId);
+
+    Optional<BuyItemList> findByItem_IdAndUser_Id(Long itemId, Long userId);
 }
